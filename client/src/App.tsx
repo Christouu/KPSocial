2502da2f98cc15b7f9f5f62458cc1bea8b4f58a5
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-12-28 20:44:48 +0200

    added a fake protected route, going to change it later with backend jwt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Leftbar from "./components/Leftbar/Leftbar";
@@ -14,6 +15,8 @@ import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 
 function App() {
+  const currentUser = false;
+
   //this layour is for repeating components
   //we are going to use navbar, left and right bar on almost every page , so it's a bad practise to repeat them on very page
   //we are using Outlet component from react-router-dom to achive this affect
@@ -31,11 +34,23 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }: any) => {
+    //this children will be our protected routes
+    //home page and profile page
+    if (!currentUser) return <Navigate to={"/login"} />;
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
       //here we are connecting our repeating components to our routers
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       //here we are adding the children if there are any to our routes
       //this seems like angular routing... (meh angular)
       children: [
