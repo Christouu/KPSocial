f84f77629fc2a2c30785bf5e52d287b618cbe4db
Kristou <kristou.svetoslavov.petkov@gmail.com>
2023-01-04 21:22:02 +0200

    added tests for Login page for now

--- client/src/pages/Login/Login.test.tsx
+++ client/src/pages/Login/Login.test.tsx
@@ -19,4 +19,41 @@ describe("Tests for Login Page", () => {
     expect(span).toBeInTheDocument();
     expect(span.textContent).toBe("Don't have an account?");
   });
+
+  test("Test if button Register is in the document and it's text is Register", () => {
+    render(<Login />, { wrapper: MemoryRouter });
+    const buttonElement = screen.getByRole("button", { name: "Register" });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement.textContent).toEqual("Register");
+  });
+
+  test("Test if form is in the document", () => {
+    render(<Login />, { wrapper: MemoryRouter });
+    const form = screen.getByTestId("form");
+    expect(form).toBeInTheDocument();
+  });
+
+  test("Test if there are 2 input elements adn check their placeholder text", () => {
+    render(<Login />, { wrapper: MemoryRouter });
+    const inputElements = screen.getAllByTestId("input");
+    expect(inputElements).toHaveLength(2);
+
+    const username = screen.getByPlaceholderText(
+      "Username"
+    ) as HTMLInputElement;
+    //here i need as HTMLInputElement cuz it doesnt let me get placeholder prop
+    expect(username.placeholder).toBe("Username");
+
+    const password = screen.getByPlaceholderText(
+      "Password"
+    ) as HTMLInputElement;
+    expect(password.placeholder).toBe("Password");
+  });
+
+  test("Test if form has a button with text Login", () => {
+    render(<Login />, { wrapper: MemoryRouter });
+    const formButton = screen.getByRole("button", { name: "Login" });
+    expect(formButton).toBeInTheDocument();
+    expect(formButton.textContent).toBe("Login");
+  });
 });
